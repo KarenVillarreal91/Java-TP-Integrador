a1be44b36583a03cde62c3357a0dfb3e5d083213
Karen Villarreal <karenvillarreal9150@gmail.com>
2023-10-21 01:04:59 -0300

    JS Entregado

---
+++ compra.js
@@ -0,0 +1,33 @@
+
+function traerResumen(){
+    let cantidad = document.getElementById('cantidad').value;
+    let descuento = document.getElementById('categoria').value;
+
+    if(cantidad > 0){
+        if(descuento == '0'){
+            descuento = 0.2;
+        }
+        else if(descuento == '1'){
+            descuento = 0.5;
+        }
+        else{
+            descuento = 0.85;
+        }
+    
+        let precioTotal = 200 * cantidad;
+        mostrarResumen(precioTotal * descuento);
+    }
+}
+
+function mostrarResumen(total){
+    document.getElementById('totalTexto').innerHTML = 'Total a Pagar: $' + total;
+    document.getElementById('totalDiv').removeAttribute('hidden');
+}
+
+function borrar(){
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('correo').value = '';
+    document.getElementById('cantidad').value = '';
+    document.getElementById('totalDiv').setAttribute('hidden', true);
+}
